a7af6c84693df75274f2b54bd9bff0e266b57164
vinodandroid <vinod@dolphinwebsolution.com>
2019-01-28 15:21:39 +0530

    vinod iteam_details and wishlist row and java

--- app/src/main/java/com/sismatix/iheal/Fragments/Wishlist_fragment.java
+++ app/src/main/java/com/sismatix/iheal/Fragments/Wishlist_fragment.java
@@ -3,29 +3,29 @@ package com.sismatix.iheal.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.sismatix.iheal.R;
 
-import me.relex.circleindicator.CircleIndicator;
-
 /**
  * A simple {@link Fragment} subclass.
  */
-public class iteam_details extends Fragment {
-    ViewPager mPager;
-    CircleIndicator indicator;
+public class Wishlist_fragment extends Fragment {
+
+
+    public Wishlist_fragment() {
+        // Required empty public constructor
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v=inflater.inflate(R.layout.fragment_iteam_details, container, false);
-        mPager = (ViewPager) v.findViewById(R.id.pager);
-        indicator = (CircleIndicator) v.findViewById(R.id.indicator);
+
+        View v=inflater.inflate(R.layout.fragment_wishlist_fragment, container, false);
 
         return v;
     }
