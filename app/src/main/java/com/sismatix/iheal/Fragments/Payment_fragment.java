4d771f75e8bfc66212b14b3db849783b01e19588
nehalgohil <nehaldolphin@gmail.com>
2019-02-15 11:25:24 +0530

    Nehal Changes.. Cartlist login check

--- app/src/main/java/com/sismatix/iheal/Fragments/Payment_fragment.java
+++ app/src/main/java/com/sismatix/iheal/Fragments/Payment_fragment.java
@@ -45,7 +45,6 @@ public class Payment_fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         v= inflater.inflate(R.layout.fragment_payment, container, false);
         AllocateMEmory(v);
         CALL_CART_DELIVERY();
